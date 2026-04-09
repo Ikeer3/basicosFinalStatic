0f1830196f84f10d4f3099273a31b6572ed1ce09
Iker Santos Insua <ikersantosinsua@gmail.com>
2026-04-09 13:16:40 +0200

    Añadido Main

--- src/Circulo/MainCirculo.java
+++ src/Circulo/MainCirculo.java
@@ -3,6 +3,7 @@ package Circulo;
 public class MainCirculo {
     public static void main(String[] args) {
 
-        
+        Circulo circulo1 = new Circulo();
+        System.out.printf("\nEl area del circulo es: %.2f", circulo1.areaCirculo());
     }
 }
